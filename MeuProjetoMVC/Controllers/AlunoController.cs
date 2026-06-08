054da2dfa5eadd50fb080aa8822771603f55da81
igorsfcfernandes2004-sys <igorsfcfernandes2004@uni9.edu.br>
2026-06-07 21:04:49 -0300

    Add Index action to AlunoController

--- MeuProjetoMVC/Controllers/AlunoController.cs
+++ MeuProjetoMVC/Controllers/AlunoController.cs
@@ -4,6 +4,13 @@ namespace MeuProjetoMVC.Controllers
 {
     public class AlunoController : Controller
     {
+    public IActionResult Index()
+        {
+            ViewBag.Nome = "Igor";
+            ViewBag.Curso = "Análise e Desenvolvimento de Sistemas";
+            ViewBag.Semestre = "1º Semestre";
 
+            return View();
+        }
     }
 }
